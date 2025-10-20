ea05340d89271175daee1831907c6fdd88005b31
MuriloBC2 <murilobarrosdocarmo@gmai.com>
2025-10-20 20:44:48 -0300

    continuação da tela de inicio

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     return (
     <>
     <div className="bg-[#E7DAD1] w-screen h-20 flex flex-row items-center justify-between">
-        <div className="flex items-center justify-center border-[#E7DAD1] ml-20 rounded-full shadow-[1px_9px_8px_3px_rgba(0,_0,_0,_0.3)]">
+        <div className="flex items-center justify-center border-[#E7DAD1] ml-30 rounded-full shadow-[1px_9px_8px_3px_rgba(0,_0,_0,_0.3)]">
             <img src={logo} alt="Logo" className="h-18"/>
         </div>
-        <div className="flex flex-row gap-15 mr-70 text-[20px] font-normal font-bold text-[#6F4F28]  ">
+        <div className="flex flex-row gap-15 mr-40 text-[20px] font-[just] font-thin text-[#6F4F28]  ">
            <button>Inicio</button>
            <button>Menu</button>
            <button>Sobre Nós</button>
--- my-app/src/pages/inicial.jsx
+++ my-app/src/pages/inicial.jsx
@@ -8,14 +8,17 @@ const Inicial = () => {
   return (
  <>
     <Navbar/>
-   <div  className="h-screen w-screen  flex justify-center items-center bg-[url('assets/arte_fundo_inicio.png')] bg-opacity-25 bg-cover flex flex-row items-center justify-center">
-   <div className="flex flex-col w-200 h-150 border-2 text-[#C0977D] font-bold ">
-    <h1 className="text-[60px] ">O pão que você ama, agora <b>entregue em minutos</b></h1>
-    <p>Com o <b>Minha Sacola</b>, você seleciona seus pães, cafés e doces favoritos e nosso motoqueiro leva o sabor até você em minutos</p>
-    <div>
-        <button>Minha Sacola</button>
-        <button>Como Funcioa ?</button>
+   <div  className="h-screen w-screen  flex justify-center items-center bg-[url('assets/arte_fundo_inicio.png')] bg-cover flex flex-row items-center justify-center">
+   <div className="flex flex-col w-160 h-150  text-[#C0977D] font-bold font-[just] font-thin ">
+    <h1 className="text-[60px] ">O pão que você ama, agora <span className="text-[#6F4F28]">entregue em minutos</span></h1>
+    <p className="text-[24px]">Com o <span className="text-[#6F4F28]">Minha Sacola</span>, você seleciona seus pães, cafés e doces favoritos e nosso motoqueiro leva o sabor até você em minutos</p>
+    <div className=" mt-20 space-x-5">
+        <button className="w-[255px] h-[64px]  bg-[#9B5C42] text-white">Minha Sacola</button>
+        <button className="w-[255px] h-[64px]  bg-[#C0977D] text-white">Como Funcioa ?</button>
     </div>
+   </div>
+   <div className="ml-10  mb-20">
+      <img src={cafe_inicio} alt="Logo" className="h-[572px] w-[570px]"/>
    </div>
     
    </div>
